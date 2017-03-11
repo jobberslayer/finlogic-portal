5c8d320c08c220f420e833bfb8765e0a0bb12a17
Kevin Lester <kevin@e-kevin.com>
2017-03-11 10:52:50 -0500

    latest

--- db/migrate/20170309201015_add_column_statement_type_to_statements.rb
+++
@@ -1,5 +0,0 @@
-class AddColumnStatementTypeToStatements < ActiveRecord::Migration[5.0]
-  def change
-    add_column :statements, :statement_type, :string
-  end
-end
--- scripts/add_user.rb
+++ scripts/add_user.rb
@@ -2,17 +2,19 @@ require 'optparse'
 
 o = {}    
 OptionParser.new do |opt|    
+  opt.on('--org_name LOCATION_NAME') { |x| o[:org_name] = x }    
   opt.on('--loc_name LOCATION_NAME') { |x| o[:loc_name] = x }    
   opt.on('--first FNAME') { |x| o[:fname] = x }    
   opt.on('--last LNAME') { |x| o[:lname] = x }    
   opt.on('--email EMAIL') { |x| o[:email] = x }    
   opt.on('--role ROLE') { |x| o[:role] = x }    
+  opt.on('--send_email') {  o[:send_email] = true }    
   opt.on('--myhelp') { puts opt; exit }    
 end.parse!
 
 password = '11223344556677'
 
-l = Location.by_name(o[:loc_name])
+l = Location.by_name(o[:org_name], o[:loc_name])
 
 unless l
   puts "Could not find location #{o[:loc_name]}"
@@ -42,3 +44,7 @@ u.information = i
 i.save
 u.save
 l.save
+
+if o[:send_email]
+  u.send_reset_password_instructions
+end
