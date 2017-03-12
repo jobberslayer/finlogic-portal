a1ef433475c487de9631e211c440a140babf0afc
Kevin Lester <kevin@e-kevin.com>
2017-03-12 18:00:45 -0400

    using path strings now in json statement

--- app/models/statement.rb
+++ app/models/statement.rb
@@ -12,7 +12,7 @@ class Statement < ApplicationRecord
     return Statement.where(location_id: location.id, statement_type: TYPE_BALANCE).order('created_at').last
   end
   
-  def to_array(skip_zero_amts=false, remove_empty_headers=false, condense=false)
+  def to_array_old(skip_zero_amts=false, remove_empty_headers=false, condense=false)
     # no sense doing the rest of this if condensed is on
     if condense
       remove_empty_headers = false
@@ -48,6 +48,49 @@ class Statement < ApplicationRecord
     else
       return mydata
     end
+  end
+
+  def to_array(skip_zero_amts=false, remove_empty_headers=false, condense=false)
+    # no sense doing the rest of this if condensed is on
+    if condense
+      remove_empty_headers = false
+    end
+
+    mydata = []
+    s_data = JSON.parse(self.statement_data)
+    s_data.each do |h|
+      next if skip_zero_amts && !h["amount"].nil? && h["amount"].to_f == 0.0
+      next if condense && h["col"] >= 4 && !h["key"].downcase.starts_with?('total')
+      myhash = {
+        key:    h["key"],
+        col:    h["col"].to_i,
+        amount: h["amount"].nil? ? nil : h["amount"].to_f,
+        indent: h["indent"].to_i,
+        path:   h["path"]
+      }
+      pp myhash
+      mydata.push myhash
+    end
+
+    return mydata
+
+    # if remove_empty_headers
+    #   i=0
+    #   filtered = []
+    #   mydata.each do |h|
+    #     next_entry = i + 1
+    #     if !mydata[next_entry].nil? && h[:amount].blank? && mydata[next_entry][:col] <= h[:col]
+    #       i += 1
+    #       next
+    #     else
+    #       filtered.push h
+    #     end
+    #     i += 1
+    #   end
+    #   return filtered
+    # else
+    #   return mydata
+    # end
   end
 
 end
--- scripts/load_statement.rb
+++ scripts/load_statement.rb
@@ -1,5 +1,6 @@
 require 'optparse'
 require 'csv'
+require 'pp'
 require Rails.root.join('lib/string').to_s
 
 o = {}    
@@ -42,23 +43,12 @@ i = 1
 csv[4..csv.size].each do |row|
   name = nil
   col = nil
-  if !row[0].blank?
-    name = row[0]
-    col = 1
-  elsif !row[1].blank?
-    name = row[1] 
-    col = 2
-  elsif !row[2].blank?
-    name = row[2] 
-    col = 3
-  elsif !row[3].blank?
-    name = row[3] 
-    col = 4
-  elsif !row[4].blank?
-    name = row[4] 
-    col = 5
-  else
-    next
+  (0..start_data_col-1).each do |c|
+    if !row[c].blank?
+      name = row[c]
+      col = c + 1
+      break
+    end
   end
 
   house_num = start_data_col 
@@ -91,7 +81,68 @@ csv[4..csv.size].each do |row|
   i += 1
 end
 
+DELIM = '|'
+
+def add_path(path, piece, place)
+  path += "#{DELIM}#{piece}"
+  while path.count(DELIM) < place do
+    path = "#{DELIM}#{path}"
+  end
+  return path
+end
+
+def back_path(path, diff) 
+  if diff > 0
+    parts = path.split(DELIM, -1)
+    (1..diff).each do |x|
+      parts.pop
+    end
+    return parts.join(DELIM)
+  else
+    return path
+  end
+end
+
+path = ''
+last_col = 0
 houses.each do |house|
+  path_struct = []
+  d = house_data[house]
+  d.each do |h|
+    if h[:col].to_i > last_col
+      if h[:amount].blank?
+        path = add_path(path, h[:key], h[:col].to_i)
+      else
+        h[:path] = path
+        h[:indent] = path.path_child_indent
+        path_struct.push h
+      end
+      last_col = h[:col].to_i
+    elsif h[:col].to_i < last_col
+      path = back_path(path, last_col - h[:col].to_i)
+      if !h[:amount].blank?
+        h[:path] = path
+        h[:indent] = path.path_child_indent
+        path_struct.push h
+      end
+      last_col = h[:col].to_i
+    else
+      if h[:amount].blank?
+        #path = back_path(path, last_col - 1 ) 
+        path = add_path(path, h[:key], h[:col].to_i)
+      else
+        h[:path] = path
+        h[:indent] = path.path_child_indent
+        path_struct.push h
+      end
+      last_col = h[:col].to_i
+    end
+  end
+  house_data[house] = path_struct
+end
+
+houses.each do |house|
+  #pp house_data[house].to_json
   statement_objs[house].statement_data = house_data[house].to_json
 end
 
