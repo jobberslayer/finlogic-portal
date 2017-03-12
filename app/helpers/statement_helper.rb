3a8d37466c628a46b308ed8efa5240b2ff8f8ae5
Kevin Lester <kevin@e-kevin.com>
2017-03-12 18:00:45 -0400

    using path strings now in json statement

--- app/helpers/statement_helper.rb
+++ app/helpers/statement_helper.rb
@@ -2,4 +2,9 @@ module StatementHelper
   def statement_line(line)
     render 'statement_line', line: line
   end
+
+  def statement_indent(indent)
+    p indent if indent < 0
+    return ('&nbsp;'*(indent*5)).html_safe
+  end
 end
--- lib/string.rb
+++ lib/string.rb
@@ -2,4 +2,12 @@ class String
   def to_col_name
     self.downcase.gsub(/[^0-9a-z]/i, '_').gsub(/_{1,}/, '_')
   end
+
+  def path_indent
+    self.split('|', -1).count 
+  end
+
+  def path_child_indent
+    self.split('|', -1).count + 1 
+  end
 end
