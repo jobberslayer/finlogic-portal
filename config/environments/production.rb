0c47a8b7583e0113404a29d6d5163b61c0cd166b
Kevin Lester <kevin@e-kevin.com>
2017-03-15 20:46:13 +0000

    email configuration needed to go in production config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -61,6 +61,21 @@ Rails.application.configure do
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
 
+  MYCONFIG = YAML.load_file("#{Rails.root.to_s}/config/config.yml")[Rails.env]
+
+  config.action_mailer.delivery_method = :smtp
+  # SMTP settings for gmail
+  config.action_mailer.smtp_settings = {
+   :address              => "smtp.gmail.com",
+   :port                 => 587,
+   :user_name            => MYCONFIG['gmail_username'],
+   :password             => MYCONFIG['gmail_password'],
+   :authentication       => "plain",
+  :enable_starttls_auto => true
+  }
+
+  Rails.application.routes.default_url_options[:host] = MYCONFIG['hostname'] 
+
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
   config.i18n.fallbacks = true
