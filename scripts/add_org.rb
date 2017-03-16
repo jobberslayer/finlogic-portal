4a8fbafd906943e9395a80381278855c76d12847
Kevin Lester <kevin@e-kevin.com>
2017-03-16 19:48:29 +0000

    fixed issues if entities already exist.

--- scripts/add_org.rb
+++ scripts/add_org.rb
@@ -1,20 +1,25 @@
 require 'optparse'
 
-o = {}    
-OptionParser.new do |opt|    
-  opt.on('--name ORG_NAME') { |x| o[:name] = x }    
-  opt.on('--address ADDRESS') { |x| o[:address] = x }    
-  opt.on('--city CITY') { |x| o[:city] = x }    
-  opt.on('--state STATE') { |x| o[:state] = x }    
-  opt.on('--zip ZIP') { |x| o[:zip] = x }    
-  opt.on('--contact CONTACT_NAME') { |x| o[:contact] = x }    
-  opt.on('--email CONTACT_EMAIL') { |x| o[:email] = x }    
-  opt.on('--phone CONTACT_PHONE') { |x| o[:phone] = x }    
-  opt.on('--myhelp') { puts opt; exit }    
-end.parse! 
+o = {}
+OptionParser.new do |opt|
+  opt.on('--name ORG_NAME') { |x| o[:name] = x }
+  opt.on('--address ADDRESS') { |x| o[:address] = x }
+  opt.on('--city CITY') { |x| o[:city] = x }
+  opt.on('--state STATE') { |x| o[:state] = x }
+  opt.on('--zip ZIP') { |x| o[:zip] = x }
+  opt.on('--contact CONTACT_NAME') { |x| o[:contact] = x }
+  opt.on('--email CONTACT_EMAIL') { |x| o[:email] = x }
+  opt.on('--phone CONTACT_PHONE') { |x| o[:phone] = x }
+  opt.on('--myhelp') { puts opt; exit }
+end.parse!
+
+if Organization.find_by(name: o[:name])
+  p "Organization already exists"
+  exit
+end
 
 o = Organization.new({
-  name: o[:name], 
+  name: o[:name],
   address: o[:address],
   city: o[:city],
   state: o[:state],
--- scripts/add_user.rb
+++ scripts/add_user.rb
@@ -1,15 +1,15 @@
 require 'optparse'
 
-o = {}    
-OptionParser.new do |opt|    
-  opt.on('--org_name LOCATION_NAME') { |x| o[:org_name] = x }    
-  opt.on('--loc_name LOCATION_NAME') { |x| o[:loc_name] = x }    
-  opt.on('--first FNAME') { |x| o[:fname] = x }    
-  opt.on('--last LNAME') { |x| o[:lname] = x }    
-  opt.on('--email EMAIL') { |x| o[:email] = x }    
-  opt.on('--role ROLE') { |x| o[:role] = x }    
-  opt.on('--send_email') {  o[:send_email] = true }    
-  opt.on('--myhelp') { puts opt; exit }    
+o = {}
+OptionParser.new do |opt|
+  opt.on('--org_name LOCATION_NAME') { |x| o[:org_name] = x }
+  opt.on('--loc_name LOCATION_NAME') { |x| o[:loc_name] = x }
+  opt.on('--first FNAME') { |x| o[:fname] = x }
+  opt.on('--last LNAME') { |x| o[:lname] = x }
+  opt.on('--email EMAIL') { |x| o[:email] = x }
+  opt.on('--role ROLE') { |x| o[:role] = x }
+  opt.on('--send_email') {  o[:send_email] = true }
+  opt.on('--myhelp') { puts opt; exit }
 end.parse!
 
 password = '11223344556677'
@@ -32,10 +32,15 @@ unless i
 end
 l.informations.push i
 
+if User.find_by(email: o[:email])
+  p "Email already exists"
+  exit
+end
+
 u = User.new({
   :email => o[:email], :password => password, :password_confirmation => password })
 unless u
-  puts u.errors  
+  puts u.errors
   exit
 end
 
