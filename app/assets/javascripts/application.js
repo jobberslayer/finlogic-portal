dbce2757c2a0e86e7d7967e6ca9677192ea15aec
Kevin Lester <kevin@e-kevin.com>
2017-03-16 17:33:48 +0000

    got js worked out with dumb tether bug and turbolinks

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,7 +12,9 @@
 //
 //= require jquery
 //= require jquery_ujs
+//= require tether
 //= require bootstrap-sprockets
 //= require turbolinks
 //= require_tree .
-//= require_tree ./application
+//= require_tree ./mystuff
+//
