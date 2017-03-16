7dab224874b668d7497920991d002d906778bbe0
Kevin <kevin@e-kevin.com>
2017-03-16 15:14:04 -0400

    added org logos and icons support

--- app/helpers/statement_helper.rb
+++ app/helpers/statement_helper.rb
@@ -7,4 +7,12 @@ module StatementHelper
     p indent if indent < 0
     return ('&nbsp;'*(indent*5)).html_safe
   end
+
+  def statement_org_name(location)
+    if @location.organization.logo.nil?
+      render 'statement_plain_org_name', location: @location
+    else
+      render 'statement_logo_org_name', location: @location
+    end
+  end
 end
