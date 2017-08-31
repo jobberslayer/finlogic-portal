7a733c9d7c5a1a1dc9308b875537c99a8dde8b58
Kevin Lester <kevin@e-kevin.com>
2017-08-31 13:37:32 -0400

    merge

--- db/migrate/20170316191228_aoi_images.rb
+++ db/migrate/20170316191228_aoi_images.rb
@@ -1,6 +1,10 @@
 class AoiImages < ActiveRecord::Migration[5.0]
   def change
     o = Organization.by_name('aoii')
+    if o.nil?
+      o = Organization.new
+      o.name = 'aoii'
+    end
     o.icon = 'Alpha-Omicron-Pi-Rose.png'
     o.logo = 'aoii_logo.png'
     o.save
